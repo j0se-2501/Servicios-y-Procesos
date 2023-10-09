98acffb784ef1cd2fb23a827e4fd6d383ad1791e
j0se-2501 <jolufer23@gmail.com>
2023-10-09 20:58:27 +0200

    ejercicio 1

---
+++ Actividad1Cifrado/src/ejercicio1/Principal.java
@@ -0,0 +1,14 @@
+package ejercicio1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		SinCifrado uno = new SinCifrado();
+		uno.pedirDatos();
+		uno.annadirTxt();
+		uno.comprobarDatos();
+
+	}
+
+}
--- Actividad1Cifrado/src/ejercicio1/SinCifrado.java
+++ Actividad1Cifrado/src/ejercicio1/SinCifrado.java
@@ -1,7 +1,90 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class SinCifrado {
 	
+	String user;
+	String pass;
+	Scanner sc = new Scanner (System.in);
+	File archivo;
+	FileReader fr;
+	BufferedReader br;
+	FileWriter fw;
+	BufferedWriter bw;
 	
+	public SinCifrado(String user, String pass){
+		
+		this.user=user;
+		this.pass=pass;
+		
+	}
+	
+	public SinCifrado(){
+		
+		
+	}
+	
+	public void pedirDatos() {
+		System.out.println("Usuario:");
+		user = sc.next();
+		System.out.println("Contrasenna:");
+		pass = sc.next();
+	}
+	
+	public void annadirTxt() {
+		
+		try {
+			
+			archivo = new File("userList.txt");
+			fw = new FileWriter(archivo);
+			bw = new BufferedWriter (fw);
+			bw.write("User: "+user+", ");
+			bw.write("Pass: "+pass+";\n");
+			bw.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void comprobarDatos() {
+		
+		try {
+			
+			System.out.println("Comprobacion de datos:");
+			pedirDatos();
+			archivo = new File("userList.txt");
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+			String dato;
+			while ((dato = br.readLine()) != null) {
+				 
+					if (dato.equals("User: "+user+", Pass: "+pass+";")) { 
+						System.out.println("Datos correctos"); 
+					} else
+						System.out.println("Datos incorrectos");
+			}
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != fr) {
+					fr.close();
+					fw.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
 
 }
