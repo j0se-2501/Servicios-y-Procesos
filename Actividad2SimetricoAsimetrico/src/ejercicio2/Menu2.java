49bf0fd34c3f3c52c4d0d817f88e651ce1336744
j0se-2501 <jolufer23@gmail.com>
2023-10-25 15:14:41 +0200

    comentarios ejercicio 2, cifrado asim

--- Actividad2SimetricoAsimetrico/src/ejercicio2/Menu2.java
+++ Actividad2SimetricoAsimetrico/src/ejercicio2/Menu2.java
@@ -25,12 +25,12 @@ public static void MostrarMenu() {
 	
 	generarKeysAsimetricas();
 	
-	int opcion=-1; //menu para elegir si se aporta una clave o se genera una
+	int opcion=-1; //menu para que el usuario elija una opcion
 	
 	while(opcion!=0) {
 	
 	System.out.println("\n---------------------------------------------\n\n"
-			+ "Clave publica: "+Base64.getEncoder().encodeToString(parejaClaves.getPublic().getEncoded())+"\n"
+			+ "Clave publica: "+Base64.getEncoder().encodeToString(parejaClaves.getPublic().getEncoded())+"\n" //se pasa de Key a String para mostrarse correctamente por pantalla
 			+ "Clave privada: "+Base64.getEncoder().encodeToString(parejaClaves.getPrivate().getEncoded())+"\n\n"
 			+ "1. Cifrar Mensaje.\n"
 			+ "2. Descifrar mensaje.\n"
@@ -40,9 +40,9 @@ public static void MostrarMenu() {
 	
 	opcion = sc.nextInt();
 	
-	try {
+	try { 
 	
-	switch(opcion) {
+	switch(opcion) { //switch con las opciones del menu
 	case 1: { cifrarMensaje();
 			break;
 			}
@@ -55,7 +55,7 @@ public static void MostrarMenu() {
 	default: System.out.println("Introduzca una opcion correcta.");
 	}
 	
-	} catch (InputMismatchException e) {
+	} catch (InputMismatchException e) { //catch para el caso en que no meta un entero
 		
 	}
 	
@@ -68,20 +68,15 @@ public static void generarKeysAsimetricas() {
 	
 	try {
 	
-	//Creating KeyPair generator object
+	keyPairGen = KeyPairGenerator.getInstance("RSA"); //primero llamo el KeyPairGenerator, le meto el algoritmo RSA
 	
-	keyPairGen = KeyPairGenerator.getInstance("RSA");
-	
-    //Initializing the KeyPairGenerator
-    keyPairGen.initialize(512);
+    keyPairGen.initialize(512); //lo inicializo con un tamaño de 512 bits; es el que menor me permite, para que las claves no ocupen demasiado en pantalla
     
-    //Generating the pair of keys
-    parejaClaves = keyPairGen.generateKeyPair();
+    parejaClaves = keyPairGen.generateKeyPair(); //aqui se genera la pareja de claves
     
     System.out.println("Claves generadas.");
 	
 	} catch (NoSuchAlgorithmException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
 	
@@ -89,42 +84,33 @@ public static void generarKeysAsimetricas() {
 
 public static void cifrarMensaje() {
 	
-	if (parejaClaves == null) System.out.println("\nEs necesario generar primero la pareja de claves."); 
+	if (parejaClaves == null) System.out.println("\nEs necesario generar primero la pareja de claves."); //en caso de que no haya claves generadas, aunque luego cambie el codigo y aqui es imposible llegar en teoria, pero por si acaso lo dejo
 	
 	else {
 	
 	System.out.println("Introduzca un mensaje para cifrar:");
 	
 	sc.nextLine();
-	mensaje=sc.nextLine();
-	
-	//Creating a Cipher object
+	mensaje=sc.nextLine(); //mensaje del usuario para cifrar
     
 	try {
 		
-		Cipher cipher;
+		Cipher cipher; //creo un objeto de la clase Cipher y le meto el algoritmo RSA
 		cipher = Cipher.getInstance("RSA");
 	
-      
-    //Initializing a Cipher object
-    cipher.init(Cipher.ENCRYPT_MODE, parejaClaves.getPublic()); //se encripta con la clave publica;
-	System.out.println("Clave publica: "+Base64.getEncoder().encodeToString(parejaClaves.getPublic().getEncoded()));
+    cipher.init(Cipher.ENCRYPT_MODE, parejaClaves.getPublic()); //inicializo el objeto cipher y se encripta con la clave publica;
+	System.out.println("Clave publica: "+Base64.getEncoder().encodeToString(parejaClaves.getPublic().getEncoded())); //muestro la clave publica por pantalla
     
-    //encrypting the data
-    mensajeCifrado = cipher.doFinal(mensaje.getBytes());	 
-    System.out.println("Mensaje cifrado: "+new String(Base64.getEncoder().encodeToString(mensajeCifrado)));
+    mensajeCifrado = cipher.doFinal(mensaje.getBytes()); //aquí se encripta a un array de bytes	 
+    System.out.println("Mensaje cifrado: "+new String(Base64.getEncoder().encodeToString(mensajeCifrado))); //muestro el array de bytes encriptado por pantalla
 	
 	} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	} catch (InvalidKeyException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	} catch (IllegalBlockSizeException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	} catch (BadPaddingException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
     
@@ -133,37 +119,35 @@ public static void cifrarMensaje() {
 
 public static void descifrarMensaje() {
 	
-	if (mensajeCifrado==null) System.out.println("Es necesario cifrar primero un mensaje");
+	//no dejo introducir un mensaje cualquiera al usuario ya que se necesita haber cifrado antes las claves;
+	//se recoge el mensaje cifrado del metodo cifrarMensaje();
+	//por ende, el usuario necesitaria primero cifrar un mensaje, y copiar y pegar luego el mensaje cifrado y las claves que salieran por pantalla.
+	//para evitar complicaciones y por facilidad de uso, prefiero dejarlo asi.
 	
+	if (mensajeCifrado==null) System.out.println("Es necesario cifrar primero un mensaje"); //if en caso de que no se haya cifrado un mensaje.
+																						
 	else {
 		
-	System.out.println("Mensaje cifrado: "+new String(Base64.getEncoder().encodeToString(mensajeCifrado)));
+	System.out.println("Mensaje cifrado: "+new String(Base64.getEncoder().encodeToString(mensajeCifrado))); //muestro por pantalla el mensaje cifrado para confirmar que es el mismo
     
 	try {
 		
 		Cipher cipher;
-		cipher = Cipher.getInstance("RSA");
+		cipher = Cipher.getInstance("RSA"); //genero un objeto cipher y le paso el argumento rsa
 	
-      
-    //Initializing a Cipher object
-    cipher.init(Cipher.DECRYPT_MODE, parejaClaves.getPrivate()); //se desencripta con la clave privada;
-	System.out.println("Clave privada: "+Base64.getEncoder().encodeToString(parejaClaves.getPrivate().getEncoded()));
+    cipher.init(Cipher.DECRYPT_MODE, parejaClaves.getPrivate()); //se inicializa y se desencripta con la clave privada;
+	System.out.println("Clave privada: "+Base64.getEncoder().encodeToString(parejaClaves.getPrivate().getEncoded())); //muestro la clave privada por pantalla
     
-    //encrypting the data
-    byte[] bytes = cipher.doFinal(mensajeCifrado);	 
-    System.out.println("Mensaje descifrado: "+new String(bytes, StandardCharsets.UTF_8));
+    byte[] bytes = cipher.doFinal(mensajeCifrado);//descifro el mensaje a un array de bytes
+    System.out.println("Mensaje descifrado: "+new String(bytes, StandardCharsets.UTF_8)); //y lo paso a string para mostrarlo pon pantalla
 	
 	} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	} catch (InvalidKeyException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	} catch (IllegalBlockSizeException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	} catch (BadPaddingException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
 	
